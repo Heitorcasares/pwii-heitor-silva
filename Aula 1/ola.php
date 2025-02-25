1f88b2cb40c538582a00272cb1bb1f4eb441d390
Heitor <suzanacasares@gmail.com>
2025-02-25 16:32:09 -0300

    Aula de PHP com exemplos de concatenação de variaveis

---
+++ Aula 1/ola.php
@@ -0,0 +1,17 @@
+<?php
+    // echo é o print, mas também tem ECHO ou EcHo
+     echo "Hello World!"; 
+
+     // para criar variáveis precisa colocar um sifrão antes do nome da variável
+
+     $lindeza = "João Siles";
+     $nota = 11;
+     $ui = "melhor professor";
+
+    // ${nome da variável} referência a variável
+    // . é contatenar
+    // você pode juntar html com php como mostrados nos exemplos abaixo
+    echo "${lindeza} <br>";
+    echo  $lindeza . " é nota " . $nota;
+    echo  $lindeza . " <p> " . "é nota " . $nota . " e é o " . $ui . " <p> ";
+?>
